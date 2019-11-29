ecbe11476039142abf4da9f03a6b95c654873585
Emalee <emalee.keesler@gmail.com>
2019-11-28 21:33:12 -0600

    admin added

--- pages/pages/urls.py
+++ pages/pages/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path
 
 from .views import HomePageView, AboutPageView, SearchPageView # new
+from . import views
 
 urlpatterns = [
     path('search/', SearchPageView.as_view(), name='search'), # new
     path('about/', AboutPageView.as_view(), name='about'), # new
     path('', HomePageView.as_view(), name='home'),  
+    path('', views.index, name='index'),
 ]
--- pages/pages/views.py
+++ pages/pages/views.py
@@ -1,11 +1,13 @@
 # pages/views.py
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
-
-
 class AboutPageView(TemplateView): # new
     template_name = 'about.html'
 
