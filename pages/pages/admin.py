b6ad5adb5bd61128555cb81e207a325a929eea45
RubenOr <49449377+RubenOr@users.noreply.github.com>
2019-11-29 20:00:11 -0600

    added tables to admin page

--- pages/pages/admin.py
+++ pages/pages/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import *
+from .models import Book, Author, Category, ContactDetail, Address, Email, Customer, Order, OrderItem, Phone, Supplier, SupplierRep
 
 #admin.site.register(database)
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(ContactDetail)
+admin.site.register(Address)
+admin.site.register(Email)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Phone)
+admin.site.register(Supplier)
+admin.site.register(SupplierRep)
--- pages/pages/models.py
+++ pages/pages/models.py
@@ -8,15 +8,20 @@ class ContactDetail(models.Model):
     
     def __str__(self):
         return '{0}'.format(self.ContactID)
+
 class Address(models.Model):
-    cid = models.ForeignKey(ContactDetail, on_delete=models.CASCADE, primary_key=True)
+    cid = models.ForeignKey(ContactDetail, on_delete=models.CASCADE)
     addr = models.CharField(max_length=45, primary_key=True)
 
     def __str__(self):
         return '{0}, {1}'.format(self.cid, self.addr)
+
 class Email(models.Model):
-    cid = models.ForeignKey(ContactDetail,on_delete=models.CASCADE, primary_key=True)
+    cid = models.ForeignKey(ContactDetail, on_delete=models.CASCADE)
     email = models.CharField(max_length=45, primary_key=True)
+
+    def __str__(self):
+        return '{0}: {1}'.format(self.cid, self.email)
 
 
 class Author(models.Model):
@@ -32,13 +37,13 @@ class Author(models.Model):
     
 class Category(models.Model):
     CategoryCode = models.IntegerField(primary_key=True, unique=True)
-    CategoryDescription = models.CharField(max_length=45)
+    CategoryDescription = models.CharField(max_length=45, unique=True)
 
     def __str__(self):
         return self.CategoryDescription
 
 class Book(models.Model):
-    isbn = models.IntegerField(primary_key=True, unique=True)
+    isbn = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=45)
     publish_date = models.DateField()
     price = models.DecimalField(max_digits=3,decimal_places=2)
@@ -48,8 +53,9 @@ class Book(models.Model):
         return self.title
 
     
-class Customers(models.Model):
-    CustomerID = models.IntegerField(primary_key=True,unique=True, auto_created=True)
+class Customer(models.Model):
+    CustomerID = models.AutoField(primary_key=True)
+    # CustomerID = models.IntegerField(primary_key=True,unique=True, auto_created=True)
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
     ContactID = models.ForeignKey(ContactDetail, on_delete=models.CASCADE)
@@ -59,15 +65,16 @@ class Customers(models.Model):
 
 
 class Order(models.Model):
-    OrderID = models.IntegerField(primary_key=True, unique=True, auto_created=True)
+    OrderID = models.AutoField(primary_key=True)
+    # OrderID = models.IntegerField(primary_key=True, unique=True, auto_created=True)
     Order_Date = models.DateField()
     Order_Value = models.IntegerField()
-    cid = models.ForeignKey(Customers, on_delete=models.CASCADE, )
+    cid = models.ForeignKey(Customer, on_delete=models.CASCADE, )
 
     def __str__(self):
         return '{0}'.format(self.OrderID)
 
-class OrderItems(models.Model):
+class OrderItem(models.Model):
     ItemNumber = models.IntegerField(primary_key=True)
     Item_Price = models.IntegerField()
     OrderID = models.ForeignKey(Order, on_delete=models.CASCADE)
@@ -82,3 +89,23 @@ class Phone(models.Model):
 
     def __str__(self):
         return '{0}'.format(self.number)
+
+class Supplier(models.Model):
+    Name = models.CharField(max_length=45, primary_key=True)
+
+    def __str__(self):
+        return self.Name
+
+class SupplierRep(models.Model):
+    sid = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    cell_number = models.CharField(max_length=13)
+    work_number = models.CharField(max_length=13)
+    email = models.CharField(max_length=45)
+
+    class Meta:
+        unique_together = (('first_name','last_name' ),)
+
+    def __str__(self):
+        return '{0}, {1} {2}'.format(self.sid, self.first_name, self.last_name)
