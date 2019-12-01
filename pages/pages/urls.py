4b2e65fd718309edc937ccc559971f21894b2986
RubenOr <49449377+RubenOr@users.noreply.github.com>
2019-11-30 19:10:26 -0600

    Update urls.py

--- pages/pages/urls.py
+++ pages/pages/urls.py
@@ -3,13 +3,12 @@ from django.urls import path
 
 # from .views import HomePageView, AboutPageView, SearchPageView # new
 from . import views
-from .views import Book
+from .views import BookListView
 
 urlpatterns = [
-    # path('search/', name='search'), # new
+    
     path('', views.home, name='pages-home'),
     path('about/', views.about, name='pages-about'), # new
-    path('browse/', views.browse, name='pages-browse'),
-    path('browse/', views.browse, name='pages-browse'),
-
+    path('browse/', BookListView.as_view(), name='pages-browse'),
+    
 ]
